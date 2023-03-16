ffb6625ee1f7a42c5fd351d38d422a18b235ba18
Manas15-coder <manas15072002@gmail.com>
2023-03-17 00:03:43 +0530

    first commit

---
+++ Learn Js/script.js
@@ -0,0 +1,41 @@
+/*var a =10;
+var b= 50;
+
+const sum=a+b;
+
+console.log(sum)
+
+var a = "cbdbcdhbchd"
+console.log(a)
+
+
+var a = 1;
+var name="Manas"
+var DataArr=[1,2,4,5,6]
+var test= true;
+var c = null;
+console.log(array)
+var array = ["1","2","3","sbhsbhs","schsvchs"]
+
+console.log(a)
+
+console.log(name)
+
+console.log(DataArr)
+
+console.log(test)
+console.log(typeof(a))
+
+console.log(typeof())
+
+console.log(c)
+*/
+
+var a=10
+
+function hello(){
+    var b =20
+    console.log(b);
+}
+console.log(a);
+hello();
